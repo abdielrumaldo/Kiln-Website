b02be1df0d86af3f90fcbb9d4382f4ec2711131a
abdielrumaldo <abdielrumaldo@users.noreply.github.com>
2019-02-24 16:28:04 -0600

    Added modbus functionality
    Signed-off-by: abdielrumaldo <7h3unkn0wn1412@gmail.com>

--- C Files/controller.c
+++ C Files/controller.c
@@ -1,1 +1,26 @@
-// File will contain configuration form python into C
+//Create a new RTU context with proper serial parameters (in this example,
+//device name /dev/ttyS0, baud rate 9600, no parity bit, 8 data bits, 1 stop bit)
+#include <modbus/modbus-rtu.h>
+#include <modbus/modbus.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+
+    modbus_t *ctx = modbus_new_rtu("/dev/ttyS0", 9600, 'N', 8, 1);
+
+    if (!ctx) {
+        fprintf(stderr, "Failed to create the context: %s\n", modbus_strerror(errno));
+        exit(1);
+    }
+
+    if (modbus_connect(ctx) == -1) {
+        fprintf(stderr, "Unable to connect: %s\n", modbus_strerror(errno));
+        modbus_free(ctx);
+        exit(1);
+    }
+    return 0;
+}
+
